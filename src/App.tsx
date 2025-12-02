5dc5d8e85d201da278c0fbb0f9b3aee727846788
Varun Mahagaokar <varun090295@gmail.com>
2025-12-01 19:01:01 -0500

    reorganised imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
-import "./App.css";
-import AboutUs from "./components/AboutUs/AboutUs";
 import ContactBanner from "./components/Banner/ContactBanner";
+import AppNavbar from "./components/Navbar/AppNavbar";
+import Hero from "./components/Hero/Hero";
+import AboutUs from "./components/AboutUs/AboutUs";
 import OfferBanner from "./components/Banner/OfferBanner";
 import Courses from "./components/Course/Courses";
 import CourseRegistration from "./components/CourseRegisteration/CourseRegistration";
 import Footer from "./components/Footer/Footer";
-import Hero from "./components/Hero/Hero";
-import AppNavbar from "./components/Navbar/AppNavbar";
+
 import Testimonials from "./components/Testimonials/Testimonials";
 import RoadTestVideo from "./components/Video/RoadTestVideo";
+import "./App.css";
 
 function App() {
   return (
